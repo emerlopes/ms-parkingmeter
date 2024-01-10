0b376b0b634eee338573b919bf0e6f3ef337f465
Emerson <lopes.ds@live.com>
2024-01-09 21:03:34 -0300

    feat() initial commit

---
+++ src/main/java/com/techchallenge/msparkingmeter/MsParkingmeterApplication.java
@@ -0,0 +1,13 @@
+package com.techchallenge.msparkingmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsParkingmeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsParkingmeterApplication.class, args);
+    }
+
+}
