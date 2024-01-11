9fb9608aee344cb708d7ca9489891d40da8d94d6
Emerson <lopes.ds@live.com>
2024-01-11 08:45:14 -0300

    chore() add properties

---
+++ src/main/java/com/techchallenge/msparkingmeter/infrastructure/properties/Properties.java
@@ -0,0 +1,26 @@
+package com.techchallenge.msparkingmeter.infrastructure.properties;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class Properties {
+
+    // APPLICATION
+    @Value("${application.local.zone.sp}")
+    private String localZoneSp;
+
+    // SNS
+    @Value("${aws.sns.drivernotification.arn}")
+    private String snsDriverNotificationArn;
+
+    // SQS
+    @Value("${aws.sqs.drivernotification.arn}")
+    private String sqsDriverNotificationArn;
+
+    // ROLE
+    @Value("${aws.role.scheduler.arn}")
+    private String schedulerRoleArn;
+}
