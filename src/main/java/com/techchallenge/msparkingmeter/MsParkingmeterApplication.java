3a76839272cd0f863fd74463dae1887581d9edbb
Emerson <lopes.ds@live.com>
2024-01-27 18:16:58 -0300

    chore() update EnableFeignClients

--- src/main/java/com/techchallenge/msparkingmeter/MsParkingmeterApplication.java
+++ src/main/java/com/techchallenge/msparkingmeter/MsParkingmeterApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients({"com.techchallenge.msparkingmeter.infrastructure.*"})
+@EnableFeignClients({"com.techchallenge.msparkingmeter.infrastructure.*", "com.techchallenge.msparkingmeter.repositories.*"})
 public class MsParkingmeterApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsParkingmeterApplication.class, args);
     }
