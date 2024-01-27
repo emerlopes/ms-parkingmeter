a07b171386d902c969502f80381dca5619a7b344
Emerson <lopes.ds@live.com>
2024-01-27 18:18:51 -0300

    chore() removendo secrets

--- src/main/java/com/techchallenge/msparkingmeter/repositories/drivernotification/DriverNotificationServiceImpl.java
+++ src/main/java/com/techchallenge/msparkingmeter/repositories/drivernotification/DriverNotificationServiceImpl.java
@@ -3,6 +3,7 @@ package com.techchallenge.msparkingmeter.repositories.drivernotification;
 import com.techchallenge.msparkingmeter.domain.entity.scheduler.SchedulerInput;
 import com.techchallenge.msparkingmeter.domain.sevice.scheduler.IDriverNotificationDomainService;
 import com.techchallenge.msparkingmeter.infrastructure.properties.Properties;
+import com.techchallenge.msparkingmeter.repositories.hashicorp.HashicorpClientService;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -15,30 +16,30 @@ import java.time.ZonedDateTime;
 @Service
 public class DriverNotificationServiceImpl implements IDriverNotificationDomainService {
 
-    private final Properties properties;
+    private final HashicorpClientService hashicorpClientService;
 
-    public DriverNotificationServiceImpl(Properties properties) {
-        this.properties = properties;
+    public DriverNotificationServiceImpl(HashicorpClientService hashicorpClientService) {
+        this.hashicorpClientService = hashicorpClientService;
     }
 
     @Override
     public void createScheduledNotification(SchedulerInput input) {
 
-        Twilio.init(properties.getAccountSid(), properties.getAuthToken());
+        final var secrets = hashicorpClientService.getHashiCorpSecret();
+
+        Twilio.init(secrets.getAccountSid(), secrets.getAuthToken());
 
         final var scheduledTime = this.createScheduledTime(input);
         final var phoneNumber = this.formatPhoneNumber(input.getPhoneNumber());
 
         final var message = Message.creator(
                         new PhoneNumber(phoneNumber),
-                        properties.getMessageServiceSid(),
+                        secrets.getMessageServiceSid(),
                         input.getMessage())
                 .setSendAt(
                         ZonedDateTime.of(scheduledTime, ZoneId.of("America/Sao_Paulo")))
                 .setScheduleType(Message.ScheduleType.FIXED)
                 .create();
-
-        System.out.println(message.getSid());
     }
 
     private LocalDateTime createScheduledTime(SchedulerInput input) {
