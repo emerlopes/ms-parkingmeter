da0d9da0541e17122d39220085fc05631988f6a9
Emerson <lopes.ds@live.com>
2024-01-28 01:35:40 -0300

    chore() criando classe para calcular o valor do estacionamento

---
+++ src/main/java/com/techchallenge/msparkingmeter/domain/usecase/parkingcontrol/impl/ExecuteCalculationFinalAmountToBePaidImpl.java
@@ -0,0 +1,11 @@
+package com.techchallenge.msparkingmeter.domain.usecase.parkingcontrol.impl;
+
+import com.techchallenge.msparkingmeter.domain.entity.parkingcontrol.ParkingControlDomainEntityInput;
+import com.techchallenge.msparkingmeter.domain.usecase.parkingcontrol.ExecuteCalculationFinalAmountToBePaid;
+
+public class ExecuteCalculationFinalAmountToBePaidImpl implements ExecuteCalculationFinalAmountToBePaid {
+    @Override
+    public void execute(ParkingControlDomainEntityInput domainObject) {
+        
+    }
+}
