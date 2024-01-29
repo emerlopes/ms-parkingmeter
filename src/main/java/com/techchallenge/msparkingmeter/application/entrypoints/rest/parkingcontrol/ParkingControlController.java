88af81a55183552ee71014b996af754d1538e6be
Emerson <lopes.ds@live.com>
2024-01-29 01:56:31 -0300

    chore() criando documentacao

--- src/main/java/com/techchallenge/msparkingmeter/application/entrypoints/rest/parkingcontrol/ParkingControlController.java
+++ src/main/java/com/techchallenge/msparkingmeter/application/entrypoints/rest/parkingcontrol/ParkingControlController.java
@@ -2,12 +2,18 @@ package com.techchallenge.msparkingmeter.application.entrypoints.rest.parkingcon
 
 import com.techchallenge.msparkingmeter.application.mappers.parkingcontrol.ParkingControlMapper;
 import com.techchallenge.msparkingmeter.application.mappers.parkingcontrolperiodtype.ParkingControlPeriodTypeMapper;
+import com.techchallenge.msparkingmeter.domain.entity.parkingcontrol.ParkingControlDomainEntityOutput;
+import com.techchallenge.msparkingmeter.domain.shared.CustomData;
 import com.techchallenge.msparkingmeter.domain.usecase.parkingcontrol.ExecuteCalculationFinalAmountToBePaid;
 import com.techchallenge.msparkingmeter.domain.usecase.parkingcontrol.IExecuteFindParkingControlByIdUseCase;
 import com.techchallenge.msparkingmeter.domain.usecase.parkingcontrol.IExecuteSaveParkingControlUseCase;
 import com.techchallenge.msparkingmeter.domain.usecase.parkingcontrol.IExecuteUpdateParkingControlUseCase;
 import com.techchallenge.msparkingmeter.domain.usecase.parkingcontrolperiodtype.IExecuteFindParkingControlPeriodTypeByIdUseCase;
 import com.techchallenge.msparkingmeter.repositories.msdrivers.IDriversClient;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +26,7 @@ import java.util.UUID;
  */
 @RestController
 @RequestMapping("/api/parking-control")
+@Tag(name = "Controle de Estacionamento", description = "API para Gerenciamento de Controle de Estacionamento")
 public class ParkingControlController {
 
     private final IExecuteSaveParkingControlUseCase executeSaveParkingControlUseCase;
@@ -47,12 +54,19 @@ public class ParkingControlController {
      * Endpoint para salvar informações de controle de estacionamento.
      *
      * @param externalDriverId  ID externo do motorista.
-     * @param periodTypeId      ID do tipo de período de controle.
+     * @param periodTypeId      ID do tipo de período de controle. Use 1 para período FIXO e 2 para período VARIÁVEL.
      * @param durationInMinutes Duração em minutos (opcional) do estacionamento.
      * @return ResponseEntity com os detalhes da operação de salvar controle de estacionamento.
      */
+    @Operation(
+            summary = "Salvar informações de controle de estacionamento",
+            description = "Salva informações de controle de estacionamento com os parâmetros fornecidos. Use 1 para período FIXO e 2 para período VARIÁVEL."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação bem-sucedida, informações de controle de estacionamento salvas com sucesso.")
+    })
     @PostMapping("/{externalDriverId}")
-    public ResponseEntity<?> saveParkingControl(
+    public ResponseEntity<CustomData<ParkingControlDomainEntityOutput>> saveParkingControl(
             @PathVariable UUID externalDriverId,
             @RequestParam(value = "period-type-id") Long periodTypeId,
             @RequestParam(value = "requested-minutes", required = false) Optional<Integer> durationInMinutes) {
@@ -76,8 +90,16 @@ public class ParkingControlController {
      * @param parkingControlId ID do controle de estacionamento a ser encerrado.
      * @return ResponseEntity com os detalhes do cálculo do valor final a ser pago.
      */
+    @Operation(
+            summary = "Encerrar controle de estacionamento e calcular valor final",
+            description = "Encerra um controle de estacionamento e calcula o valor final a ser pago."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida, valor final calculado com sucesso.")
+    })
     @PutMapping("/{parkingControlId}")
-    public ResponseEntity<?> terminateParking(
+    public ResponseEntity<
+            CustomData<ParkingControlDomainEntityOutput>> terminateParking(
             @PathVariable Long parkingControlId) {
 
         final var result = executeCalculationFinalAmountToBePaid.execute(parkingControlId);
--- src/main/java/com/techchallenge/msparkingmeter/application/entrypoints/rest/parkingcontrolperiodtype/ParkingControlPeriodTypeController.java
+++ src/main/java/com/techchallenge/msparkingmeter/application/entrypoints/rest/parkingcontrolperiodtype/ParkingControlPeriodTypeController.java
@@ -1,18 +1,27 @@
 package com.techchallenge.msparkingmeter.application.entrypoints.rest.parkingcontrolperiodtype;
 
 import com.techchallenge.msparkingmeter.application.mappers.parkingcontrolperiodtype.ParkingControlPeriodTypeMapper;
+import com.techchallenge.msparkingmeter.domain.entity.parkingcontrolperiodtype.ParkingControlPeriodTypeDomainEntityOutput;
+import com.techchallenge.msparkingmeter.domain.shared.CustomData;
 import com.techchallenge.msparkingmeter.domain.usecase.parkingcontrolperiodtype.IExecuteFindAllParkingControlPeriodTypeUseCase;
 import com.techchallenge.msparkingmeter.domain.usecase.parkingcontrolperiodtype.IExecuteFindParkingControlPeriodTypeByIdUseCase;
 import com.techchallenge.msparkingmeter.domain.usecase.parkingcontrolperiodtype.IExecuteSaveParkingControlPeriodTypeUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Controlador responsável por gerenciar operações relacionadas aos tipos de período de controle de estacionamento.
  */
 @RestController
 @RequestMapping("/api/parking-control-period-type")
+@Tag(name = "Tipos de Período de Controle de Estacionamento", description = "API para Gerenciamento de Tipos de Período de Controle de Estacionamento")
 public class ParkingControlPeriodTypeController {
 
     private final IExecuteSaveParkingControlPeriodTypeUseCase executeSavePeriodTypeUseCase;
@@ -22,8 +31,8 @@ public class ParkingControlPeriodTypeController {
     /**
      * Construtor da classe ParkingControlPeriodTypeController.
      *
-     * @param executeSavePeriodTypeUseCase               Caso de uso para salvar informações de tipos de período de controle de estacionamento.
-     * @param executeFindAllParkingControlPeriodTypeUseCase Caso de uso para buscar todos os tipos de período de controle de estacionamento.
+     * @param executeSavePeriodTypeUseCase                   Caso de uso para salvar informações de tipos de período de controle de estacionamento.
+     * @param executeFindAllParkingControlPeriodTypeUseCase  Caso de uso para buscar todos os tipos de período de controle de estacionamento.
      * @param executeFindParkingControlPeriodTypeByIdUseCase Caso de uso para buscar informações de tipos de período de controle de estacionamento por ID.
      */
     public ParkingControlPeriodTypeController(IExecuteSaveParkingControlPeriodTypeUseCase executeSavePeriodTypeUseCase, IExecuteFindAllParkingControlPeriodTypeUseCase executeFindAllParkingControlPeriodTypeUseCase, IExecuteFindParkingControlPeriodTypeByIdUseCase executeFindParkingControlPeriodTypeByIdUseCase) {
@@ -38,8 +47,15 @@ public class ParkingControlPeriodTypeController {
      * @param parkingControlPeriodTypeId ID do tipo de período de controle de estacionamento.
      * @return ResponseEntity com os detalhes da operação de salvar tipo de período de controle de estacionamento.
      */
+    @Operation(
+            summary = "Salvar informações de um tipo de período de controle de estacionamento",
+            description = "Salva informações de um tipo de período de controle de estacionamento com base no ID fornecido."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação bem-sucedida, informações de tipo de período de controle de estacionamento salvas com sucesso.")
+    })
     @PostMapping("/{parking-control-period-type-id}")
-    public ResponseEntity<?> saveParkingControlPeriodType(
+    public ResponseEntity<CustomData<ParkingControlPeriodTypeDomainEntityOutput>> saveParkingControlPeriodType(
             @PathVariable("parking-control-period-type-id") Long parkingControlPeriodTypeId) {
 
         final var input = ParkingControlPeriodTypeMapper.mapToParkingControlPeriodTypeDomainEntityInput(parkingControlPeriodTypeId);
@@ -52,8 +68,15 @@ public class ParkingControlPeriodTypeController {
      *
      * @return ResponseEntity com a lista de tipos de período de controle de estacionamento encontrados.
      */
+    @Operation(
+            summary = "Buscar todos os tipos de período de controle de estacionamento",
+            description = "Busca todos os tipos de período de controle de estacionamento disponíveis."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida, lista de tipos de período de controle de estacionamento encontrada.")
+    })
     @GetMapping
-    public ResponseEntity<?> findAllParkingControlPeriodTypes() {
+    public ResponseEntity<CustomData<List<ParkingControlPeriodTypeDomainEntityOutput>>> findAllParkingControlPeriodTypes() {
         final var output = executeFindAllParkingControlPeriodTypeUseCase.execute();
         return ResponseEntity.status(HttpStatus.OK).body(output);
     }
@@ -64,8 +87,15 @@ public class ParkingControlPeriodTypeController {
      * @param parkingControlPeriodTypeId ID do tipo de período de controle de estacionamento.
      * @return ResponseEntity com os detalhes do tipo de período de controle de estacionamento encontrado.
      */
+    @Operation(
+            summary = "Buscar informações de um tipo de período de controle de estacionamento por ID",
+            description = "Busca informações de um tipo de período de controle de estacionamento com base no ID fornecido."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida, informações de tipo de período de controle de estacionamento encontradas.")
+    })
     @GetMapping("/{parking-control-period-type-id}")
-    public ResponseEntity<?> findParkingControlPeriodTypeById(
+    public ResponseEntity<CustomData<ParkingControlPeriodTypeDomainEntityOutput>> findParkingControlPeriodTypeById(
             @PathVariable("parking-control-period-type-id") Long parkingControlPeriodTypeId) {
 
         final var input = ParkingControlPeriodTypeMapper.mapToParkingControlPeriodTypeDomainEntityInput(parkingControlPeriodTypeId);
